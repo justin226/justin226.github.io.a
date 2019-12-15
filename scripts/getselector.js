7d68f80deb2538f673f07dc1e8b83340d07924cc
justin226 <56492118+justin226@users.noreply.github.com>
2019-12-15 14:07:05 -0800

    Update getselector.js

--- scripts/getselector.js
+++ scripts/getselector.js
@@ -1,5 +1,11 @@
 function get(q){
   if(q.slice(0, 1) == "#"){
-     
+    return document.getElementById(q.slice(1, q.length));
   }
-}
+  if(q.slice(0, 1) == "."){
+    return document.getElementsByClassName(q.slice(1, q.length));
+  }
+  if(q.slice(0, 1) !== "#" && q.slice(0, 1) !== "."){
+    return document.getElementsByTagName(q);
+  }
+} 
