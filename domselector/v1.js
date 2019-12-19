56e8738cf1d0596fcf748414e23e01558dae8d28
justin226 <56492118+justin226@users.noreply.github.com>
2019-12-19 10:36:34 -0800

    Update v1.js

--- domselector/v1.js
+++ domselector/v1.js
@@ -9,36 +9,3 @@ function get(q){
     return document.getElementsByTagName(q);
   }
 }
-var opGet = {
-  remove: function(query){
-    try{
-      document.body.removeChild(document.getElementById(query));
-    }
-    catch(err){
-      try{
-        document.head.removeChild(document.getElementById(query));
-      }
-      catch(err){
-        document.removeChild(document.getElementById(query));
-      }
-    }
-  },
-  hide: function(query){
-    document.getElementById(query).style.display = "none";
-	},
-  showBlock: function(query){
-    document.getElementById(query).style.display = "block";
-  },
-  showInline: function(query){
-    document.getElementById(query).style.display = "inline";
-  },
-  showInlineBlock: function(query){
-    document.getElementById(query).style.display = "inline-block";
-  },
-  val: function(query, val){
-    document.getElementById(query).value = val;
-  },
-  html: function(query, inner){
-    document.getElementById(query).innerHTML = inner;
-  }
-}
